1cbe8902ca4a4e18150c04c3c0cbcfb053eb0c0f
grynevych89 <grynevych89@gmail.com>
2026-09-07 09:41:22 +0200

    improve function: Added non_negative_number argument so the function can be reused

--- oppgave1.py
+++ oppgave1.py
@@ -1,19 +1,20 @@
 # 1.1
 
-def read_non_negative(prompt):
+def read_non_negative(prompt, non_negative_number):
     while True:
         try:
             value = int(input(prompt))
         except ValueError:
             print('Please enter valid number')
             continue
-        if value > 0:
+        if value >= non_negative_number:
             return value
         print('Please enter valid number')
 
 def calculation_of_time_spent():
-    number_of_study_sessions = read_non_negative('Number of study sessions? ')
-    minutes_per_session = read_non_negative('Minutes per session? ')
+    non_negative_number = 1
+    number_of_study_sessions = read_non_negative('Number of study sessions? ', non_negative_number)
+    minutes_per_session = read_non_negative('Minutes per session? ', non_negative_number)
 
     total_time_spent = number_of_study_sessions * minutes_per_session
     hours = total_time_spent // 60
