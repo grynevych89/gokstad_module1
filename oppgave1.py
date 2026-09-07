11c36f915131bb79c5fb3d11cc1a6974d71a8822
grynevych89 <grynevych89@gmail.com>
2026-09-07 12:05:40 +0200

    done 1.3

--- oppgave1.py
+++ oppgave1.py
@@ -11,6 +11,7 @@ def read_non_negative(prompt, non_negative_number):
             return value
         print('Please enter valid number')
 
+
 def calculation_of_time_spent():
     non_negative_number = 1
     number_of_study_sessions = read_non_negative('Number of study sessions? ', non_negative_number)
@@ -20,6 +21,7 @@ def calculation_of_time_spent():
     hours = total_time_spent // 60
     minutes = total_time_spent % 60
     print('Total time spent: ' + str(hours) + ' hours, ' + str(minutes) + ' minutes')
+
 
 # calculation_of_time_spent()
 
@@ -34,6 +36,7 @@ def read_text(prompt='Enter text: ') -> str:
             continue
         return text_example
 
+
 def text_analysis():
     text_example = read_text()
     print(text_example)
@@ -44,5 +47,34 @@ def text_analysis():
     print(text_example[::-1])
     print('python' in text_example.lower())
 
+
 # example: Hello World!@Python
-text_analysis()
+# text_analysis()
+
+
+# 1.3
+
+def is_integer(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Error: both values must be integers.")
+
+
+def numeric_range_analysis():
+    start_number = is_integer("Enter start number: ")
+    end_number = is_integer("Enter end number: ")
+    if start_number > end_number:
+        start_number, end_number = end_number, start_number
+
+    numbers = range(start_number, end_number + 1)
+    evens = [n for n in numbers if n % 2 == 0]
+    div_by_three = [n for n in numbers if n % 3 == 0]
+    total_sum = sum(numbers)
+    print(evens)
+    print(div_by_three)
+    print(total_sum)
+
+
+numeric_range_analysis()
